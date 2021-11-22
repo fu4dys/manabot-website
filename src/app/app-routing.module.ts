a91440e1c532415d30a7f5f70aefe32715ffdc90
fu4dys <fuadysyukur01@gmail.com>
2021-11-22 13:44:43 +0700

    fill card.component.hmtl

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CardComponent } from './card/card.component';
+import { CardsDatabaseComponent } from './cards-database/cards-database.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'cards', component:CardsDatabaseComponent},
+  {path:'card', component:CardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardsDatabaseComponent } from './cards-database/cards-database.component';
+import { NavComponent } from './nav/nav.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsDatabaseComponent,
+    NavComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
